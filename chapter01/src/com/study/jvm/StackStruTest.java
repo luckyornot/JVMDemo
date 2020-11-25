63e0134fea46770cb196b6eac2146397be80dddb
amadman <466456543@qq.com>
2020-11-25 20:06:55 +0800

    first-01-jvm与java体系结构

---
+++ chapter01/src/com/study/jvm/StackStruTest.java
@@ -0,0 +1,24 @@
+package com.study.jvm;
+
+/**
+ * @author wang
+ * @create 2020-11-25-18:50
+ *
+ * 反编译字节码文件
+ */
+public class StackStruTest {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        //int i = 2 + 3;
+        int i = 2;
+        int j = 3;
+        int s = i + j;
+
+        //Thread.sleep(6000);
+        System.exit(0);
+
+        System.out.println("hello");
+
+    }
+}
